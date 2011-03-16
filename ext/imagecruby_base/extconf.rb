c33fe0c252ce967e36ad22d06cddbfb30af24be7
Dario <darios3@gmail.com>
2011-03-15 23:42:27 -0300

    added header and extension files

---
+++ ext/imagecruby_base/extconf.rb
@@ -0,0 +1,7 @@
+require 'mkmf'
+dir_config('imagecruby_base')
+CONFIG['CC'] = 'gcc'
+create_makefile('imagecruby_base')
+
+
+
---
+++ lib/imagecruby.rb
@@ -0,0 +1,21 @@
+=begin
+
+This file is part of the imagecruby project, http://github.com/tario/imageruby
+
+Copyright (c) 2011 Roberto Dario Seminara <robertodarioseminara@gmail.com>
+
+imagecruby is free software: you can redistribute it and/or modify
+it under the terms of the gnu general public license as published by
+the free software foundation, either version 3 of the license, or
+(at your option) any later version.
+
+imagecruby is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.  see the
+gnu general public license for more details.
+
+you should have received a copy of the gnu general public license
+along with imagecruby.  if not, see <http://www.gnu.org/licenses/>.
+
+=end
+require "imagecruby_base"
